5e7a0374b1eb54c64cd12c83baa42e7cc8feb011
klype <49646203+SpyroMF@users.noreply.github.com>
2022-08-29 08:50:46 +0200

    Add files via upload

--- Oppgaver/Oppgave 5 Konvertering/celsius_til_farenheit.py
+++ Oppgaver/Oppgave 5 Konvertering/celsius_til_farenheit.py
@@ -12,10 +12,13 @@ def celsius_fahrenheit(temp_celsius):
     return(temp_fahrenheit)
 
 
-
+#While løkke som tar Userinput
 while True:
+    #Her får brukeren et valg mellom å konvertere fra C° til F° eller omvendt
     valg = input("Konverter\f1. C° --> F°\f2. F° --> C°\f1 eller 2?: ")
+    #Bruleren skriver inn hvilke temperatur som skal konverteres
     temp = input("Hvilke temperatur vil du konvertere?\fTemperatur: ")
+    #Simpel if sjekk som velger mellom konverterings funksjonene
     if valg == "1":
         print(celsius_fahrenheit(int(temp)))
     if valg == "2":
